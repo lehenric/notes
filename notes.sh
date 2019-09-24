2f5d362a86626b3e2b117f37e6a7af88e7c330a6
16-5798-1 <henrich.le@plus4u.net>
2019-09-25 01:46:08 +0200

    genesis

---
+++ notes.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+# vi:syntax=sh
+
+NOTES_DIRECTORY="${HOME}/notes/school"
+
+init() {
+  mkdir -p "$NOTES_DIRECTORY"
+
+}
+usage() {
+  cat 1>&2 << "EOF"
+Usage $0 OPTIONS [COURSE] NAME
+OPTIONS:
+  --help  prints this help
+  --init  initializes notes directory from config
+  --list COURSE   list course notes
+  -d,--dir prints directory of NOTES and exits
+EOF
+}
+
+# takes COURSE and NAME as parameters
+list() {
+    COURSE="$1"
+    [[ "$COURSE" == "--" ]] && COURSE=""
+    lsd -la "$NOTES_DIRECTORY/${COURSE}"
+}
+
+# takes COURSE and NAME as parameters
+main(){
+    COURSE="$1"
+    NAME="$2"
+    mkdir -p "$NOTES_DIRECTORY/${COURSE}"
+    (
+        cd "$NOTES_DIRECTORY/${COURSE}" 
+        nvim "$NAME"
+    )
+}
+
+options=$(getopt -l "help,init,list:,,verbose,dir" -o "dvhil:" -- "$@")
+
+
+# set --:
+# If no arguments follow this option, then the positional parameters are unset. Otherwise, the positional parameters 
+# are set to the arguments, even if some of them begin with a ‘-’.
+set -euo pipefail
+eval set -- "$options"
+
+while true
+do
+  case $1 in
+    -v|--verbose)
+      export verbose=1
+      set -xv  # Set xtrace and verbose mode.
+      ;;
+    -d|--dir)
+       echo "$NOTES_DIRECTORY"
+       exit 0
+       ;;
+    -h|--help)
+       usage
+       exit 0
+       ;;
+    -l|--list)
+      list "$2"
+      exit 0
+      ;;
+    -i|--init)
+      init
+      exit 0
+      ;;
+    --)
+      shift
+      export course=$1
+      export name=$2
+      main $1 $2
+      break;;
+  esac
+  shift
+done
